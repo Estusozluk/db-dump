a0339472bd642ee00ae2d113e3d158632ca97690
Ardi Jorganxhi <73110402+ArdiJorganxhi@users.noreply.github.com>
2022-11-27 13:18:30 +0300

    Update EstuSozluk.sql

--- EstuSozluk.sql
+++ EstuSozluk.sql
@@ -1,35 +1,31 @@
 DROP DATABASE IF EXISTS EstuSozluk;
 CREATE DATABASE IF NOT EXISTS EstuSozluk;
 
-
 USE EstuSozluk;
 
-
 CREATE TABLE IF NOT EXISTS Permissions(
-	userrole INT(1) NOT NULL PRIMARY KEY,
+	userroleid INT(1) unsigned NOT NULL PRIMARY KEY,
 	rolename varchar(20) NOT NULL
 );
 
-
 CREATE TABLE IF NOT EXISTS Users(
-	username VARCHAR(30) NOT NULL PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQUE,
+	userid INT unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR(30) NOT NULL UNIQUE,
+	email VARCHAR(120) NOT NULL UNIQUE,
 	password VARCHAR(255) NOT NULL,
-	userrole INT(1) NOT NULL,
-
-	CONSTRAINT FK_UserRole FOREIGN KEY(userrole)
-	REFERENCES Permissions(userrole)
+	userroleid INT(1) unsigned NOT NULL,
+	
+	CONSTRAINT FK_UserRole FOREIGN KEY (userroleid)
+	REFERENCES Permissions(userroleid)
 );
 
-
-
-INSERT INTO `Permissions` (`userrole`, `rolename`) 
+INSERT INTO `Permissions` (`userroleid`, `rolename`) 
 VALUES (0, "BannedUser");
 
-INSERT INTO `Permissions` (`userrole`, `rolename`) 
+INSERT INTO `Permissions` (`userroleid`, `rolename`) 
 VALUES (1, "User");
 
-INSERT INTO `Permissions` (`userrole`, `rolename`) 
+INSERT INTO `Permissions` (`userroleid`, `rolename`) 
 VALUES (2, "Moderator");
 
 
@@ -39,38 +35,50 @@ VALUES (2, "Moderator");
 */
 CREATE TABLE IF NOT EXISTS Entries(
 	entryid INT unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	entryuser VARCHAR(20) NOT NULL,
+	userid int unsigned NOT NULL,
 	titlename VARCHAR(50) NOT NULL,
 	content TEXT(15000) NOT NULL,
 	writedate DATETIME NOT NULL,
 	editdate DATETIME NOT NULL,
 	
-	CONSTRAINT FK_EntryUser FOREIGN KEY (entryuser)
-	REFERENCES Users(username) ON DELETE CASCADE
+	CONSTRAINT FK_EntryUser FOREIGN KEY (userid)
+	REFERENCES Users(userid) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS LikedEntries(
 	likedentryid INT unsigned NOT NULL,
-	userliked VARCHAR(20) NOT NULL,
+	userid int unsigned NOT NULL,
 	
-	PRIMARY KEY (likedentryid, userliked),
+	PRIMARY KEY (likedentryid, userid),
 	
 	CONSTRAINT FK_LikedEntry FOREIGN KEY (likedentryid)
 	REFERENCES Entries(entryid) ON DELETE CASCADE,
 	
-	CONSTRAINT FK_UserLiked FOREIGN KEY (userliked)
-	REFERENCES Users(username) ON DELETE CASCADE
+	CONSTRAINT FK_UserLiked FOREIGN KEY (userid)
+	REFERENCES Users(userid) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS DislikedEntries(
+	dislikedentryid INT unsigned NOT NULL,
+	userid int unsigned NOT NULL,
+	
+	PRIMARY KEY (dislikedentryid, userid),
+	
+	CONSTRAINT FK_DislikedEntry FOREIGN KEY (dislikedentryid)
+	REFERENCES Entries(entryid) ON DELETE CASCADE,
+	
+	CONSTRAINT FK_UserDisliked FOREIGN KEY (userid)
+	REFERENCES Users(userid) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Followships(
-	follower VARCHAR(20) NOT NULL,
-	followed VARCHAR(20) NOT NULL,
+	follower int unsigned NOT NULL,
+	followed int unsigned NOT NULL,
 	PRIMARY KEY (follower, followed),
 	
 	CONSTRAINT FK_Follower FOREIGN KEY (follower)
-	REFERENCES Users(username) ON DELETE CASCADE,
+	REFERENCES Users(userid) ON DELETE CASCADE,
 	
 	CONSTRAINT FK_Followed FOREIGN KEY (followed)
-	REFERENCES Users(username) ON DELETE CASCADE
+	REFERENCES Users(userid) ON DELETE CASCADE
 );
-
